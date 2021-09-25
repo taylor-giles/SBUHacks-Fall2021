77d422b741576d4825a069fb2381e2e6c6fd4340
taylor-giles <taylor.g1823@gmail.com>
2021-09-24 20:49:25 -0400

    Start work on card design

--- Resource.js
+++ Resource.js
@@ -19,8 +19,10 @@ export default class Resource {
     }
 
     createCard(){
-        // Create card element
+        // Create card element as a horizontal layout div
         const card = document.createElement('div');
+        card.setAttribute('horizontal', '');  
+        card.setAttribute('layout', '');
         card.classList = 'resource-card';
 
         //Create image
@@ -29,12 +31,37 @@ export default class Resource {
         img.id = this.name + "-img"
         card.appendChild(img)
 
-        // Construct card content
-        const content = 
-            '<div class="resource-card">'+
-                ''
-            '</div>';
+        //Create vertical container to store name and description
+        const nameContainer = document.createElement('div');
+        nameContainer.setAttribute('vertical', '');
+        nameContainer.setAttribute('layout', '');
 
-        
+        //Create and add name text
+        const nameText = document.createElement('div');
+        nameText.id = this.name + "-name";
+        nameText.innerHTML = this.name;
+        nameContainer.appendChild(nameText);
+
+        //Create and add description text
+        const descText = document.createElement('div');
+        descText.id = this.name + "-desc";
+        descText.innerHTML = this.desc;
+        nameContainer.appendChild(descText);
+
+        //Add name and description div
+        card.appendChild(nameContainer);
+
+        //Create and add div for amount
+        const amtText = document.createElement('div');
+        amtText.id = this.name + "-amt";
+        amtText.innerHTML = this.amount;
+        card.appendChild(amtText);
+
+        //Create and add div for collection info/build button
+        const buildDiv = document.createElement('div');
+        buildDiv.id = this.name + "-build";
+        card.appendChild(buildDiv);
+
+        return card;
     }
 }
