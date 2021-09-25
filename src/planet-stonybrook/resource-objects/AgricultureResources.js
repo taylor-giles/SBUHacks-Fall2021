1c7a814bc9b074b814974295bb838d7c22e2fdbc
taylor-giles <taylor.g1823@gmail.com>
2021-09-25 03:59:39 -0400

    Minor changes to resources constants

--- src/planet-stonybrook/resource-objects/AgricultureResources.js
+++ src/planet-stonybrook/resource-objects/AgricultureResources.js
@@ -2,15 +2,15 @@ import Resource from "../classes/Resource.js";
 
 const AGRICULTURE_NAME = "Agriculture";
 
-export const WATER = new Resource("Water", "", "../../images/Water Card.png", "L", AGRICULTURE_NAME, null);
+export const WATER = new Resource("Water", "", "../../images/New/Water Card.png", "L", AGRICULTURE_NAME, null);
 
 const CORN_COSTS = [{"resource":WATER, "amount":2}];
-export const CORN = new Resource("Corn", "", "../../../Images/Corn Card.png", "Cobs", AGRICULTURE_NAME, CORN_COSTS);
+export const CORN = new Resource("Corn", "", "../../../Images/New/Corn Card.png", "Cobs", AGRICULTURE_NAME, CORN_COSTS);
 
 const WHEAT_COSTS = [{"resource":WATER, "amount":4}];
-export const WHEAT = new Resource("Wheat", "", "../../../Images/Wheat Card.png", "kg", AGRICULTURE_NAME, WHEAT_COSTS);
+export const WHEAT = new Resource("Wheat", "", "../../../Images/New/Wheat Card.png", "kg", AGRICULTURE_NAME, WHEAT_COSTS);
 
 const BREAD_COSTS = [{"resource":WATER, "amount":15}, {"resource":WHEAT, "amount":5}];
-export const BREAD = new Resource("Bread", "", "../../../Images/Bread Card.png", "Loaves", AGRICULTURE_NAME, BREAD_COSTS);
+export const BREAD = new Resource("Bread", "", "../../../Images/New/Bread Card.png", "Loaves", AGRICULTURE_NAME, BREAD_COSTS);
 
-export const ALL_AGRICULTURE_RESOURCES = [WATER, CORN, WHEAT, BREAD];
+export const ALL_AGRICULTURE_RESOURCES = [WATER, WHEAT, CORN, BREAD];
